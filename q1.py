217d3b59a89ddc586f09d05f3d1bd58f195bbe5f
SuRRiEs006 <43891244+SuRRiEs006@users.noreply.github.com>
2022-03-11 07:43:35 +0000

    Add files via upload
    now able to generate all the binary numbers, some even numbers have an extra 0 at the end. Not incorrect but not ideal.

--- q1.py
+++ q1.py
@@ -4,6 +4,7 @@
 
 '''
 #####IMPORTS#####
+import copy
 
 ##### CODE ######
 
@@ -11,39 +12,56 @@ class BinaryNumber:
     def __init__(self,nVal):
         self.nVal = nVal
         self.highestPower = None
+        self.countdownArray = []
 
-    def biggestSquare(self):
-        Nnum = self.nVal
+    def biggestSquare(self,localDenNum):
+        Nnum = copy.deepcopy(localDenNum)
         for x in range(0,25):
             if Nnum <= (2**x):
-                self.highestPower = (x)
+                highestPower = (x)
                 break
-        print(str(2**self.highestPower) +" is the biggest square")
-        return(None)
+        
+        Nnum = Nnum-1
+        BinaryNumber.generate_binary_numbers(self,Nnum)
+        return(highestPower)
 
 
-    def generate_binary_numbers(self):
-        self.biggestSquare()
-        binaryOutput = []
-        workingNumber = self.nVal
-        for i in range((self.highestPower+0),-1,-1):
-            if (2**i) <= workingNumber:
-                binaryOutput.append("1")
-                workingNumber = workingNumber - (2**i)
+    def generate_binary_numbers(self,denNum):
+        localDenNum = copy.deepcopy(denNum)
+        if localDenNum > 0:
+            highestPower = self.biggestSquare(localDenNum)
+            binaryOutput = []
+            
+            workingNumber = copy.deepcopy(localDenNum)
+
+            #print(str(2**highestPower) +" is the biggest square")
+            for i in range((highestPower+0),-1,-1):
+                if (2**i) <= workingNumber:
+                    binaryOutput.insert(0,"1")
+                    workingNumber = workingNumber - (2**i)
+                else:
+                    binaryOutput.insert(0,"0")
+                
+            if (localDenNum%2 == 1) & ("".join(binaryOutput) != "1"):
+                self.countdownArray.append("".join(binaryOutput)[:-1] )
             else:
-                binaryOutput.append("0")
-        print("".join(binaryOutput))
+                self.countdownArray.append("".join(binaryOutput[::-1]))
+            #print(localDenNum)
+            #print("".join(binaryOutput))
+        print(self.countdownArray)
 
 
 
 
 
 
-    def binCountDown(self):
-        for i in range(0,self.nVal):
-            print(i)
+    # def binCountDown(self):
+    #     for i in range(0,self.nVal):
+    #         print(i)
+
+
 
 
 seven =BinaryNumber(257)
 
-seven.generate_binary_numbers()
+seven.generate_binary_numbers(20)
