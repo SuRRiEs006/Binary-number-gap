eb9bde15332ba72eb94a840c68e2678c94fc1d6a
SuRRiEs006 <43891244+SuRRiEs006@users.noreply.github.com>
2022-03-14 05:27:57 +0000

    testing + menu still required, I believe logic is done

--- q1.py
+++ q1.py
@@ -1,27 +1,24 @@
 #####LIMITS#####
 '''
 - MAX NUMBER IS 2**25 WHICH SHOULD BE MORE THAN BETTER
-
 '''
 #####IMPORTS#####
 import copy
 
-
 ##### CODE ######
 
 class BinaryNumber:
-    def __init__(self,nVal):
-        self.nVal = nVal
+    def __init__(self):
+        self.nVal = None
+        self.nValBin = None
         self.countdownArray = []
+        self.gapValue = 0
 
     def reverseStr(self,stringIn):
         newReversedStr = ""
         for i in range(len(stringIn)-1,-1,-1):
             newReversedStr = newReversedStr + stringIn[i]
         return(newReversedStr)
-
-
-
 
     def generate_binary_numbers(self,decToBin):
         
@@ -38,16 +35,38 @@ class BinaryNumber:
                     currentBinaryNum = currentBinaryNum + "0"
                     localN = (localN)/2
             self.countdownArray.append(self.reverseStr(currentBinaryNum))
+            if decToBin == self.nVal:
+                self.nValBin = (self.reverseStr(currentBinaryNum))
             decToBin = decToBin-1
-            print(decToBin)
             self.generate_binary_numbers(decToBin)
 
 
+
+    def binaryGap(self,index,streak):
+        
+        if index <= len(self.nValBin)-1:
+            if self.nValBin[index] == "0":
+                streak = streak+1
+            elif self.nValBin[index] == "1":
+                #print("wrote "+str(streak))
+                if streak >= self.gapValue:
+                    #print("wrote "+str(streak))
+                    self.gapValue = streak+1
+                streak = 0
+            index = index + 1
+            self.binaryGap(index,streak)
+
+            
+      
+
     def main(self):
+        self.nVal = 22
         decToBin = copy.deepcopy(self.nVal)
         self.generate_binary_numbers(decToBin)
-        print("Output final array: "+str(self.countdownArray))
+        print("Output final array: "+str(self.nValBin))
+        self.binaryGap(0,1)
+        print(self.gapValue)
 
 
-
-BinaryNumber(20).main()
+if __name__ == "__main__":
+    BinaryNumber().main()
